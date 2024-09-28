33cf2d48a1992f43cf4ca4076947218918c106d7
juanpaz90 <juan.pazmino@thoughtworks.com>
2024-09-28 13:07:11 -0500

    Adding a new Route

--- api/main.py
+++ api/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from readBQ import read_app_info
 
-
 app = FastAPI()
 
 
-@app.get("/app_info/{app_id}-{app_department}")
+@app.get("/app_info/{app_id}:{app_department}")
 def app_info(app_id: int, app_department: str):
-    return read_app_info(app_id, app_department)
+    try:
+        return read_app_info(app_id, app_department)
+    except Exception as e:
+        return "You have to authenticate in your company portal"
+
+
+@app.get("/get_data/{department}:{num}")
+def get_data(department: str, num: int):
+    return {"Department": department, "Number": num}
 
 
 @app.get("/health")
