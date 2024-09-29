3a086508d50d1a4b1baf907e6d80f298db108c36
juanpaz90 <juan.pazmino@thoughtworks.com>
2024-09-28 19:39:45 -0500

    Improve API response

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from readBQ import read_app_info
 
 app = FastAPI()
@@ -9,13 +9,15 @@ def app_info(app_id: int, app_department: str):
     try:
         return read_app_info(app_id, app_department)
     except Exception as e:
-        return "You have to authenticate in your company portal"
+        raise HTTPException(status_code=401, detail="You have to authenticate in your company portal")
 
 
 @app.get("/get_data/{action_id}:{user_email}")
-def get_data(action_id: str, user_email: int):
-    return {"Action Id": action_id, "Email": user_email}
-
+def get_data(action_id: int, user_email: str):
+    if type(action_id) is int and type(user_email) is str:
+        return {"Action Id": action_id, "Email": user_email}
+    else:
+        raise HTTPException(status_code=400, detail="Check the data and try again")
 
 @app.get("/health")
 def health():
